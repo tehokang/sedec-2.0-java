698303b96d3dfe6f0d03c9907a86ab6e5d574490
tehokang <tehokang@tehokangs-MacBook-Pro.local>
2018-08-28 01:41:46 +0900

    added CAT, PAT and PMT

--- src/dvb/TableFactory.java
+++ src/dvb/TableFactory.java
@@ -2,8 +2,14 @@ package dvb;
 
 import base.Table;
 import dvb.tables.ApplicationInformationTable;
+import dvb.tables.ConditionalAccessTable;
+import dvb.tables.ProgramAssociationTable;
+import dvb.tables.ProgramMapTable;
 
 public class TableFactory {
+    public final static int PROGRAM_ASSOCIATION_TABLE = 0x00;
+    public final static int CONDITIONAL_ACCESS_TABLE = 0x01;
+    public final static int PROGRAM_MAP_TABLE = 0x02;
     public final static int APPLICATION_INFORMATION_TABLE = 0x74;
     public final static int UNKNOWN_TABLE = 0xff;
     
@@ -14,12 +20,17 @@ public class TableFactory {
 
         switch(table_id)
         {
+            case PROGRAM_ASSOCIATION_TABLE:
+                return new ProgramAssociationTable(buffer);
+            case CONDITIONAL_ACCESS_TABLE:
+                return new ConditionalAccessTable(buffer);
+            case PROGRAM_MAP_TABLE:
+                return new ProgramMapTable(buffer);
             /**
              * @note It is related in ETSI TS 102 809 v1.4.1
              **/
             case APPLICATION_INFORMATION_TABLE:
-                section = new ApplicationInformationTable(buffer);
-                break;
+                return new ApplicationInformationTable(buffer);
             default:
                 break;
         }
