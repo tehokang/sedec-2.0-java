6f560c8e168ddebd609b1565904e0e16067c4690
tehokang <tehokang@gmail.com>
2018-09-12 17:11:53 +0900

    rename enumeration of b60 descriptors

--- src/arib/b39/DescriptorFactory.java
+++ src/arib/b39/DescriptorFactory.java
@@ -19,17 +19,17 @@ import arib.b39.descriptors.TransportProtocolDescriptor;
 import arib.b39.descriptors.TypeDescriptor;
 
 public class DescriptorFactory {
-    public final static int APPLICATION_DESCRIPTOR = 0x8029;
-    public final static int TRANSPORT_PROTOCOL_DESCRIPTOR = 0x802a;
-    public final static int SIMPLE_APPLICATION_LOCATION_DESCRIPTOR = 0x802b;
-    public final static int APPLICATION_BOUNDARY_AND_PERMISSION_DESCRIPTOR = 0x802c;
-    public final static int AUTOSTART_PRIORITY_DESCRIPTOR = 0x802d;
-    public final static int CACHE_CONTROL_INFO_DESCRIPTOR = 0x802e;
-    public final static int RANDOMIZED_LATENCY_DESCRIPTOR = 0x802f;
-    public final static int TYPE_DESCRIPTOR = 0x801c;
-    public final static int INFO_DESCRIPTOR = 0x801d;
-    public final static int EXPIRE_DESCRIPTOR = 0x801e;
-    public final static int COMPRESSION_TYPE_DESCRIPTOR = 0x801f;
+    public final static int MPEGH_APPLICATION_DESCRIPTOR = 0x8029;
+    public final static int MPEGH_TRANSPORT_PROTOCOL_DESCRIPTOR = 0x802a;
+    public final static int MPEGH_SIMPLE_APPLICATION_LOCATION_DESCRIPTOR = 0x802b;
+    public final static int MPEGH_APPLICATION_BOUNDARY_AND_PERMISSION_DESCRIPTOR = 0x802c;
+    public final static int MPEGH_AUTOSTART_PRIORITY_DESCRIPTOR = 0x802d;
+    public final static int MPEGH_CACHE_CONTROL_INFO_DESCRIPTOR = 0x802e;
+    public final static int MPEGH_RANDOMIZED_LATENCY_DESCRIPTOR = 0x802f;
+    public final static int MPEGH_TYPE_DESCRIPTOR = 0x801c;
+    public final static int MPEGH_INFO_DESCRIPTOR = 0x801d;
+    public final static int MPEGH_EXPIRE_DESCRIPTOR = 0x801e;
+    public final static int MPEGH_COMPRESSION_TYPE_DESCRIPTOR = 0x801f;
     public final static int LINKED_PU_DESCRIPTOR = 0x8030;
     public final static int LOCKED_CACHE_DESCRIPTOR = 0x8031;
     public final static int UNLOCKED_CACHE_DESCRIPTOR = 0x8032;
@@ -42,27 +42,27 @@ public class DescriptorFactory {
                 (brw.GetCurrentBuffer()[1] & 0xff));
         
         switch ( descriptor_tag ) {
-            case APPLICATION_DESCRIPTOR:
+            case MPEGH_APPLICATION_DESCRIPTOR:
                 return new ApplicationDescriptor(brw);
-            case TRANSPORT_PROTOCOL_DESCRIPTOR:
+            case MPEGH_TRANSPORT_PROTOCOL_DESCRIPTOR:
                 return new TransportProtocolDescriptor(brw);
-            case SIMPLE_APPLICATION_LOCATION_DESCRIPTOR:
+            case MPEGH_SIMPLE_APPLICATION_LOCATION_DESCRIPTOR:
                 return new SimpleApplicationLocationDescriptor(brw);
-            case APPLICATION_BOUNDARY_AND_PERMISSION_DESCRIPTOR:
+            case MPEGH_APPLICATION_BOUNDARY_AND_PERMISSION_DESCRIPTOR:
                 return new ApplicationBoundaryAndPermissionDescriptor(brw);
-            case AUTOSTART_PRIORITY_DESCRIPTOR:
+            case MPEGH_AUTOSTART_PRIORITY_DESCRIPTOR:
                 return new AutostartPriorityDescriptor(brw);
-            case CACHE_CONTROL_INFO_DESCRIPTOR:
+            case MPEGH_CACHE_CONTROL_INFO_DESCRIPTOR:
                 return new CacheControlInfoDescriptor(brw);
-            case RANDOMIZED_LATENCY_DESCRIPTOR:
+            case MPEGH_RANDOMIZED_LATENCY_DESCRIPTOR:
                 return new RandomizedLatencyDescriptor(brw);
-            case TYPE_DESCRIPTOR:
+            case MPEGH_TYPE_DESCRIPTOR:
                 return new TypeDescriptor(brw);
-            case INFO_DESCRIPTOR:
+            case MPEGH_INFO_DESCRIPTOR:
                 return new InfoDescriptor(brw);
-            case EXPIRE_DESCRIPTOR:
+            case MPEGH_EXPIRE_DESCRIPTOR:
                 return new ExpireDescriptor(brw);
-            case COMPRESSION_TYPE_DESCRIPTOR:
+            case MPEGH_COMPRESSION_TYPE_DESCRIPTOR:
                 return new CompressionTypeDescriptor(brw);
             case LINKED_PU_DESCRIPTOR:
                 return new LinkedPuDescriptor(brw);
