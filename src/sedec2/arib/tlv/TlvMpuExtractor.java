6dc5bde017fbaf99c42585b85cce16a7bc16e7c7
tehokang <tehokang@gmail.com>
2018-11-01 20:56:16 +0900

    clean up with audio

--- src/sedec2/arib/tlv/TlvMpuExtractor.java
+++ src/sedec2/arib/tlv/TlvMpuExtractor.java
@@ -54,8 +54,8 @@ public class TlvMpuExtractor {
     protected BlockingQueue<QueueData> m_mfu_timed_texts = new ArrayBlockingQueue<QueueData>(100);
     protected BlockingQueue<byte[]> m_tlv_packets = new ArrayBlockingQueue<byte[]>(100);
     
-    protected List<MMTP_Packet> m_fragmented01_mmtp_video_mfu = new ArrayList<>();
-    protected List<MMTP_Packet> m_fragmented02_mmtp_video_mfu = new ArrayList<>();
+    protected List<MMTP_Payload_MPU> m_fragmented01_mmtp_video_mpu = new ArrayList<>();
+    protected List<MMTP_Payload_MPU> m_fragmented02_mmtp_video_mpu = new ArrayList<>();
 
     public TlvMpuExtractor() {
         m_tlv_extractor_thread = new Thread(new Runnable() {
@@ -277,18 +277,15 @@ public class TlvMpuExtractor {
      * @throws IOException 
      * @note Video Filtering
      */
-    protected void processMfuVideo(MMTP_Packet mmtp) throws InterruptedException, IOException {
+    protected void processMfuVideo(int packet_id, MMTP_Payload_MPU mpu) throws InterruptedException, IOException {
         List<MFU> mfus = null;
-        int packet_id = mmtp.getPacketId();
-        MMTP_Payload_MPU mpu = mmtp.getMPU();
         byte[] nal_prefix = {0x00, 0x00, 0x00, 0x01};
         ByteArrayOutputStream outputStreamNal = new ByteArrayOutputStream();
         ByteArrayOutputStream outputStreamRawMfu = new ByteArrayOutputStream();
 
-        Logger.d(String.format("[V] pid : 0x%04x, psn : 0x%08x, " + 
+        Logger.d(String.format("[V] pid : 0x%04x, " + 
                 "msn : 0x%08x, f_i : 0x%x, timed_flag : 0x%x, a_f : 0x%x, mfu.size : %d \n", 
-                packet_id, mmtp.getPacketSequenceNumber(),
-                mpu.getMPUSequenceNumber(), mpu.getFragmentationIndicator(),
+                packet_id, mpu.getMPUSequenceNumber(), mpu.getFragmentationIndicator(),
                 mpu.getTimedFlag(), mpu.getAggregationFlag(), mpu.getMFUList().size()));
         
         switch ( mpu.getFragmentationIndicator() ) {
@@ -305,18 +302,16 @@ public class TlvMpuExtractor {
                 put(new QueueData(packet_id, outputStreamNal.toByteArray()));
                 break;
             case 0x01:
-                m_fragmented01_mmtp_video_mfu.add(mmtp);
+                m_fragmented01_mmtp_video_mpu.add(mpu);
                 break;
             case 0x02:
-                m_fragmented02_mmtp_video_mfu.add(mmtp);
+                m_fragmented02_mmtp_video_mpu.add(mpu);
                 break;
             case 0x03:
-                for ( Iterator<MMTP_Packet> it = m_fragmented01_mmtp_video_mfu.iterator() ; 
+                for ( Iterator<MMTP_Payload_MPU> it = m_fragmented01_mmtp_video_mpu.iterator() ; 
                         it.hasNext() ; ) {
-                    MMTP_Packet mmtp01 = it.next();
-                    MMTP_Payload_MPU mpu01 = mmtp01.getMPU();
-                    if( mmtp01.getPacketId() == packet_id && 
-                            mpu01.getFragmentationIndicator() == 0x01 ) {
+                    MMTP_Payload_MPU mpu01 = it.next();
+                    if( mpu01.getFragmentationIndicator() == 0x01 ) {
                         mfus = mpu01.getMFUList();
                         for ( int i=0; i<mfus.size(); i++ ) {
                         	if(i == 0) {
@@ -332,12 +327,10 @@ public class TlvMpuExtractor {
                     }
                 }
                 
-                for ( Iterator<MMTP_Packet> it = m_fragmented02_mmtp_video_mfu.iterator() ; 
+                for ( Iterator<MMTP_Payload_MPU> it = m_fragmented02_mmtp_video_mpu.iterator() ; 
                         it.hasNext() ; ) {
-                    MMTP_Packet mmtp02 = it.next();
-                    MMTP_Payload_MPU mpu02 = mmtp02.getMPU();
-                    if( mmtp02.getPacketId() == packet_id && 
-                            mpu02.getFragmentationIndicator() == 0x02 ) {
+                    MMTP_Payload_MPU mpu02 = it.next();
+                    if( mpu02.getFragmentationIndicator() == 0x02 ) {
                         mfus = mpu02.getMFUList();
                         for ( int i=0; i<mfus.size(); i++ ) {
                             outputStreamRawMfu.write(mfus.get(i).MFU_data_byte);
@@ -350,44 +343,96 @@ public class TlvMpuExtractor {
                 for ( int i=0; i<mfus.size(); i++ ) {
                     outputStreamRawMfu.write(mfus.get(i).MFU_data_byte);
                 }
+                
                 outputStreamNal.write(outputStreamRawMfu.toByteArray());
                 put(new QueueData(packet_id, outputStreamNal.toByteArray()));
                 break;
         }
+    }
+    
+    protected void processMfuAudio(int packet_id, MMTP_Payload_MPU mpu) throws InterruptedException, IOException {
+        List<MFU> mfus = null;
+        ByteArrayOutputStream outputStreamRawMfu = new ByteArrayOutputStream();
+
+        Logger.d(String.format("[A] pid : 0x%04x, " + 
+                "msn : 0x%08x, f_i : 0x%x, timed_flag : 0x%x, a_f : 0x%x, mfu.size : %d \n", 
+                packet_id, mpu.getMPUSequenceNumber(), mpu.getFragmentationIndicator(),
+                mpu.getTimedFlag(), mpu.getAggregationFlag(), mpu.getMFUList().size()));
         
+        switch ( mpu.getFragmentationIndicator() ) {
+            case 0x00:
+                mfus = mpu.getMFUList();
+                for ( int i=0; i<mfus.size(); i++ ) {
+                    /**
+                     * @note Gathering MFU
+                     */
+                }
+                put(new QueueData(packet_id, outputStreamRawMfu.toByteArray()));
+                break;
+            case 0x01:
+                m_fragmented01_mmtp_video_mpu.add(mpu);
+                break;
+            case 0x02:
+                m_fragmented02_mmtp_video_mpu.add(mpu);
+                break;
+            case 0x03:
+                for ( Iterator<MMTP_Payload_MPU> it = m_fragmented01_mmtp_video_mpu.iterator() ; 
+                        it.hasNext() ; ) {
+                    MMTP_Payload_MPU mpu01 = it.next();
+                    if( mpu01.getFragmentationIndicator() == 0x01 ) {
+                        mfus = mpu01.getMFUList();
+                        for ( int i=0; i<mfus.size(); i++ ) {
+                            /**
+                             * @note Gathering MFU
+                             */
+                            outputStreamRawMfu.write(mfus.get(i).MFU_data_byte);
+                        }
+                        it.remove();
+                        break;
+                    }
+                }
+                
+                for ( Iterator<MMTP_Payload_MPU> it = m_fragmented02_mmtp_video_mpu.iterator() ; 
+                        it.hasNext() ; ) {
+                    MMTP_Payload_MPU mpu02 = it.next();
+                    if( mpu02.getFragmentationIndicator() == 0x02 ) {
+                        mfus = mpu02.getMFUList();
+                        for ( int i=0; i<mfus.size(); i++ ) {
+                            outputStreamRawMfu.write(mfus.get(i).MFU_data_byte);
+                        }
+                        it.remove();
+                    }
+                } 
+                
+                mfus = mpu.getMFUList();
+                for ( int i=0; i<mfus.size(); i++ ) {
+                    outputStreamRawMfu.write(mfus.get(i).MFU_data_byte);
+                }
+                put(new QueueData(packet_id, outputStreamRawMfu.toByteArray()));
+                break;
+        }
     }
     
-    protected void processMfuAudio(MMTP_Packet mmtp) throws InterruptedException, IOException {
-        /**
-         * Audio Filtering
-         */
-    }
-    
-    protected void processMfuData(MMTP_Packet mmtp) {
-        MMTP_Payload_MPU mpu = mmtp.getMPU();
+    protected void processMfuData(int packet_id, MMTP_Payload_MPU mpu) {
         /**
          * Data Filtering
          */
-        Logger.d(String.format("[D] pid : 0x%04x, ptype : 0x%x, psn : 0x%08x, " + 
-                "pc : 0x%x, msn : 0x%08x, f_i : 0x%x  \n", 
-                mmtp.getPacketId(), mmtp.getPayloadType(), mmtp.getPacketSequenceNumber(),
-                mmtp.getPacketCounter(), mpu.getMPUSequenceNumber(),
-                mpu.getFragmentationIndicator()));
     }
     
     protected void processMmtpMpu(MMTP_Packet mmtp) throws InterruptedException, IOException {
         int packet_id = mmtp.getPacketId();
+        MMTP_Payload_MPU mpu = mmtp.getMPU();
         
         if ( m_video_pid_filters.contains(packet_id) ) {
-            processMfuVideo(mmtp);
+            processMfuVideo(packet_id, mpu);
         } 
         
         if ( m_audio_pid_filters.contains(packet_id) ) {
-            processMfuAudio(mmtp);
+            processMfuAudio(packet_id, mpu);
         }
         
         if ( m_timed_text_pid_filters.contains(packet_id) ) {
-            processMfuData(mmtp);
+            processMfuData(packet_id, mpu);
         }
     }
     
