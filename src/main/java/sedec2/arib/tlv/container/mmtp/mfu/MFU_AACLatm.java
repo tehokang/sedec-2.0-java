5a88e58d314ee48f247fbbfae8f8d2138dbcf459
tehokang <tehokang@gmail.com>
2018-11-30 17:27:19 +0900

    added template of MFU_AACLatm

---
+++ src/main/java/sedec2/arib/tlv/container/mmtp/mfu/MFU_AACLatm.java
@@ -0,0 +1,11 @@
+package sedec2.arib.tlv.container.mmtp.mfu;
+
+import sedec2.base.BitReadWriter;
+
+public class MFU_AACLatm extends BitReadWriter {
+
+    public MFU_AACLatm(byte[] buffer) {
+        super(buffer);
+
+    }
+}
