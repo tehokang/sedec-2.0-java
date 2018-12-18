4eeaf553699d5c80dd3f83863a55b2cafea85c40
tehokang <tehokang@gmail.com>
2018-12-18 12:11:45 +0900

    add contractions of table

--- src/main/java/sedec2/arib/b10/TableFactory.java
+++ src/main/java/sedec2/arib/b10/TableFactory.java
@@ -62,37 +62,100 @@ import sedec2.base.UnknownTable;
  */
 public class TableFactory {
     public final static byte PROGRAM_ASSOCIATION_TABLE = (byte) 0x00;
+    public final static byte PAT = PROGRAM_ASSOCIATION_TABLE;
+
     public final static byte CONDITIONAL_ACCESS_TABLE = (byte) 0x01;
+    public final static byte CAT = CONDITIONAL_ACCESS_TABLE;
+
     public final static byte PROGRAM_MAP_TABLE = (byte) 0x02;
+    public final static byte PMT = PROGRAM_MAP_TABLE;
+
     public final static byte ACTUAL_NETWORK_INFORMATION_TABLE = (byte) 0x40;
+    public final static byte NIT_ACTUAL = ACTUAL_NETWORK_INFORMATION_TABLE;
+
     public final static byte OTHER_NETWORK_INFORMATION_TABLE = (byte) 0x41;
+    public final static byte NIT_OTHER = OTHER_NETWORK_INFORMATION_TABLE;
+
     public final static byte ACTUAL_SERVICE_DESCRIPTION_TABLE = (byte) 0x42;
+    public final static byte SDT_ACTUAL = ACTUAL_SERVICE_DESCRIPTION_TABLE;
+
     public final static byte OTHER_SERVICE_DESCRIPTION_TABLE = (byte) 0x46;
+    public final static byte SDT_OTHER = OTHER_SERVICE_DESCRIPTION_TABLE;
+
     public final static byte BOUQUET_ASSOCIATION_TABLE = (byte) 0x4a;
+    public final static byte BAT = BOUQUET_ASSOCIATION_TABLE;
+
     public final static byte ACTUAL_TS_PRESENT_EVENT_INFORMATION_TABLE = (byte) 0x4e;
+    public final static byte EIT_ACTUAL = ACTUAL_TS_PRESENT_EVENT_INFORMATION_TABLE;
+
     public final static byte OTHER_TS_PRESENT_EVENT_INFORMATION_TABLE = (byte) 0x4f;
+    public final static byte EIT_OTHER = OTHER_TS_PRESENT_EVENT_INFORMATION_TABLE;
+
     public final static byte TIME_DATE_TABLE = (byte) 0x70;
+    public final static byte TDT = TIME_DATE_TABLE;
+
     public final static byte RUNNING_STATUS_TABLE = (byte) 0x71;
+    public final static byte RST = RUNNING_STATUS_TABLE;
+
     public final static byte STUFFING_TABLE = (byte) 0x72;
+    public final static byte ST = STUFFING_TABLE;
+
     public final static byte TIME_OFFSET_TABLE = (byte) 0x73;
+    public final static byte TOT = TIME_OFFSET_TABLE;
+
     public final static byte APPLICATION_INFORMATION_TABLE = (byte) 0x74;
+    public final static byte AIT = APPLICATION_INFORMATION_TABLE;
+
     public final static byte DISCONTINUITY_INFORMATION_TABLE = (byte) 0x7e;
+    public final static byte DIT = DISCONTINUITY_INFORMATION_TABLE;
+
     public final static byte SELECTION_INFORMATION_TABLE = (byte) 0x7f;
+    public final static byte SIT = SELECTION_INFORMATION_TABLE;
+
     public final static byte ENTITLEMENT_CONTROL_MESSAGE_1 = (byte) 0x82;
+    public final static byte ECM_1 = ENTITLEMENT_CONTROL_MESSAGE_1;
+
     public final static byte ENTITLEMENT_CONTROL_MESSAGE_2 = (byte) 0x83;
+    public final static byte ECM_2 = ENTITLEMENT_CONTROL_MESSAGE_2;
+
     public final static byte ENTITLEMENT_MANAGEMENT_MESSAGE_1 = (byte) 0x84;
+    public final static byte EMM_1 = ENTITLEMENT_MANAGEMENT_MESSAGE_1;
+
     public final static byte ENTITLEMENT_MANAGEMENT_MESSAGE_2 = (byte) 0x85;
+    public final static byte EMM_2 = ENTITLEMENT_MANAGEMENT_MESSAGE_2;
+
     public final static byte DOWNLOAD_CONTROL_TABLE = (byte) 0xc0;
+    public final static byte DCT = DOWNLOAD_CONTROL_TABLE;
+
     public final static byte DOWNLOAD_TABLE = (byte) 0xc1;
+    public final static byte DT = DOWNLOAD_TABLE;
+
     public final static byte PARTIAL_CONTENT_ANNOUNCEMENT_TABLE = (byte) 0xc2;
+    public final static byte PCAT = PARTIAL_CONTENT_ANNOUNCEMENT_TABLE;
+
     public final static byte NETWORK_BOARD_INFORMATION_TABLE_1 = (byte) 0xc5;
+    public final static byte NBIT_1 = NETWORK_BOARD_INFORMATION_TABLE_1;
+
     public final static byte NETWORK_BOARD_INFORMATION_TABLE_2 = (byte) 0xc6;
+    public final static byte NBIT_2 = NETWORK_BOARD_INFORMATION_TABLE_2;
+
     public final static byte LINKED_DESCRIPTION_TABLE = (byte) 0xc7;
+    public final static byte LDT = LINKED_DESCRIPTION_TABLE;
+
     public final static byte LOCAL_EVENT_INFORMATION_TABLE = (byte) 0xd0;
+    public final static byte LEIT = LOCAL_EVENT_INFORMATION_TABLE;
+
     public final static byte EVENT_RELATION_TABLE = (byte) 0xd1;
+    public final static byte ERT = EVENT_RELATION_TABLE;
+
     public final static byte INDEX_TRANSMISSION_TABLE = (byte) 0xd2;
+    public final static byte ITT = INDEX_TRANSMISSION_TABLE;
+
     public final static byte SOFTWARE_DOWNLOAD_TRIGGER_TABLE = (byte) 0xc3;
+    public final static byte SDTT = SOFTWARE_DOWNLOAD_TRIGGER_TABLE;
+
     public final static byte BROADCASTER_INFORMATION_TABLE = (byte) 0xc4;
+    public final static byte BIT = BROADCASTER_INFORMATION_TABLE;
 
     public final static byte UNKNOWN_TABLE = (byte) 0xff;
 
--- src/main/java/sedec2/dvb/ts/si/TableFactory.java
+++ src/main/java/sedec2/dvb/ts/si/TableFactory.java
@@ -40,8 +40,13 @@ import sedec2.dvb.ts.si.tables.dsmcc.DSMCCSection;
  */
 public class TableFactory {
     public final static byte PROGRAM_ASSOCIATION_TABLE = (byte) 0x00;
+    public final static byte PAT = PROGRAM_ASSOCIATION_TABLE;
+
     public final static byte CONDITIONAL_ACCESS_TABLE = (byte) 0x01;
+    public final static byte CAT = CONDITIONAL_ACCESS_TABLE;
+
     public final static byte PROGRAM_MAP_TABLE = (byte) 0x02;
+    public final static byte PMT = PROGRAM_MAP_TABLE;
 
     public final static byte DSMCC_UN_MESSAGE_TABLE = 0x3b;
     public final static byte DSMCC_DOWNLOAD_DATA_MESSAGE_TABLE = 0x3c;
@@ -49,19 +54,47 @@ public class TableFactory {
     public final static byte DSMCC_PRIVATE_DATA_TABLE = 0x3e;
 
     public final static byte APPLICATION_INFORMATION_TABLE = (byte) 0x74;
+    public final static byte AIT = APPLICATION_INFORMATION_TABLE;
+
     public final static byte ACTUAL_NETWORK_INFORMATION_TABLE = (byte) 0x40;
+    public final static byte NIT_ACTUAL = ACTUAL_NETWORK_INFORMATION_TABLE;
+
     public final static byte OTHER_NETWORK_INFORMATION_TABLE = (byte) 0x41;
+    public final static byte NIT_OTHER = OTHER_NETWORK_INFORMATION_TABLE;
+
     public final static byte ACTUAL_SERVICE_DESCRIPTON_TABLE = (byte) 0x42;
+    public final static byte SDT_ACTUAL = ACTUAL_SERVICE_DESCRIPTON_TABLE;
+
     public final static byte OTHER_SERVICE_DESCRIPTION_TABLE = (byte) 0x46;
+    public final static byte SDT_OTHER = OTHER_SERVICE_DESCRIPTION_TABLE;
+
     public final static byte BOUQUET_ASSOCIATION_TABLE = (byte) 0x4a;
+    public final static byte BAT = BOUQUET_ASSOCIATION_TABLE;
+
     public final static byte ACTUAL_TS_PRESENT_EVENT_INFORMATION_TABLE = (byte) 0x4e;
+    public final static byte EIT_ACTUAL = ACTUAL_TS_PRESENT_EVENT_INFORMATION_TABLE;
+
     public final static byte OTHER_TS_PRESENT_EVENT_INFORMATION_TABLE = (byte) 0x4f;
+    public final static byte EIT_OTHER = OTHER_TS_PRESENT_EVENT_INFORMATION_TABLE;
+
     public final static byte TIME_DATE_TABLE = (byte) 0x70;
+    public final static byte TDT = TIME_DATE_TABLE;
+
     public final static byte RUNNING_STATUS_TABLE = (byte) 0x71;
+    public final static byte RST = RUNNING_STATUS_TABLE;
+
     public final static byte STUFFING_TABLE = (byte) 0x72;
+    public final static byte ST = STUFFING_TABLE;
+
     public final static byte TIME_OFFSET_TABLE = (byte) 0x73;
+    public final static byte TOT = TIME_OFFSET_TABLE;
+
     public final static byte DISCONTINUITY_INFORMATION_TABLE = (byte) 0x7e;
+    public final static byte DIT = DISCONTINUITY_INFORMATION_TABLE;
+
     public final static byte SELECTION_INFORMATION_TABLE = (byte) 0x7f;
+    public final static byte SIT = SELECTION_INFORMATION_TABLE;
+
     public final static byte UNKNOWN_TABLE = (byte) 0xff;
 
     public static Table createTable(byte[] buffer) {
